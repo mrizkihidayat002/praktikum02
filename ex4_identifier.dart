012d91928ba67b8d54de9af04f1226217eed19a4
mrizkihidayat002 <150129537+mrizkihidayat002@users.noreply.github.com>
2024-03-14 14:25:45 +0700

    Add files via upload

---
+++ ex4_identifier.dart
@@ -0,0 +1,7 @@
+//pengenal (identifier)
+void main(List<String> args) {
+  String nama_depan = "rizki";
+  double dua_dimensi = 3.2;
+  var tanda = '#';
+  print('nama saya $nama_depan dimensi $dua_dimensi tanda $tanda');
+}
